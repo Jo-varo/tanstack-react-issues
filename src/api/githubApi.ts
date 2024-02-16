d4af5f4abd9b6f1e4ed13e1bcabb4819fba33b34
jovaro <jorgevargas0240@gmail.com>
2024-02-16 01:50:15 -0500

    Modifica githubApi

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+// From Github->Settings->Dev Settings->Fine-grained tokens
+const AuthToken =
+  'github_pat_...';
+
+// In case you don't have the token, remove headers property
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization:
-      `Bearer ${'Github_Token'}`,
+    Authorization: `Bearer ${AuthToken}`,
   },
 });
